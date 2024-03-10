85f406ffb1ca8cad1f0dc0dcc1f71e95a96a24a0
Jared <78630856+Jared-02@users.noreply.github.com>
2024-03-10 21:01:01 +0800

    Fix: try to solve compatibility issues

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,9 @@
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+# AX3600 compatibility issues
+sed -i 's|coolsnowwolf/packages|coolsnowwolf/packages.git^11cf4447ce4f1e4f655747aadbc505bde03f19e6|' feeds.conf.default
+sed -i 's|coolsnowwolf/luci|coolsnowwolf/luci.git^380e994419aae665733796bb1ae14659936ea101|' feeds.conf.default
+sed -i 's|feed/routing.git|feed/routing.git^2134c56d12ced1c5b191b35e18f8330384860f15|' feeds.conf.default
+sed -i 's|feed/telephony.git|feed/telephony.git^8cf20bc21ceac07c4c3e56e23a3dd1596a0bf92d|' feeds.conf.default
--- diy-part2.sh
+++ diy-part2.sh
@@ -13,22 +13,19 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/files/bin/config_generate
 
-# Patchs
-
-# rm -rf feeds/luci/applications/luci-app-serverchan
-# git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git package/luci-app-serverchan
-
-# git clone -b dev https://github.com/vernesong/OpenClash.git --depth=1 package/openclash
-
 # 修改连接数
 # sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=165535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 # 修正连接数（by ベ七秒鱼ベ）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
 # themes
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
+git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+cd package/luci-app-argon-config
+git checkout 73dfdfda61c86b26b72acd17c8b03b0dd0107919
+cd ../..
 
 # 添加额外非必须软件包
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
